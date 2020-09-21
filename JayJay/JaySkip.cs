b170b3b395965ebe60ed62bdd81704567bdbf994
Jay <396835@H0591>
2020-09-21 11:42:15 +0100

    FirstChanges

---
+++ JayJay/JaySkip.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JayJay
+{
+    class JaySkip
+    {
+        private int jaySkip = 8;
+    }
+}
